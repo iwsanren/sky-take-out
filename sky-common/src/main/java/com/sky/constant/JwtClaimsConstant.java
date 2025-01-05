43acc0ac538a096b5110b3050dfc2a46e7240793
Zhaofang He <151109521+iwsanren@users.noreply.github.com>
2025-01-05 20:02:53 +0000

    sky take out initial project

--- sky-common/src/main/java/com/sky/constant/JwtClaimsConstant.java
+++ sky-common/src/main/java/com/sky/constant/JwtClaimsConstant.java
@@ -1,7 +1,7 @@
 package com.sky.constant;
 
+// JWT:  用于对应用程序上的用户进行身份验证的标记。
 public class JwtClaimsConstant {
-
     public static final String EMP_ID = "empId";
     public static final String USER_ID = "userId";
     public static final String PHONE = "phone";

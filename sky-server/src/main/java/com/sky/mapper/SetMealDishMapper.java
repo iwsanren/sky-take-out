6b88da02cd1a97854a43af7ab985851f0bdc8384
Zhaofang He <151109521+iwsanren@users.noreply.github.com>
2025-01-20 00:22:21 +0000

    add a new meal set

--- sky-server/src/main/java/com/sky/mapper/SetMealDishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetMealDishMapper.java
@@ -1,5 +1,6 @@
 package com.sky.mapper;
 
+import com.sky.entity.SetmealDish;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -13,4 +14,10 @@ public interface SetMealDishMapper {
      */
     // select set_meal_id from setMeal_dish where dish_id in (1,2,3,4);
     List<Long> getSetMealIdsByDishIds(List<Long> dishIds);
+
+    /**
+     * Batch insert the mappings between set meals and dishes into the database.
+     * @param setmealDishes
+     */
+    void insertBatch(List<SetmealDish> setmealDishes);
 }

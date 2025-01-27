f403065ec8bf81c3a6ed565a9cd251201bd5a057
Zhaofang He <151109521+iwsanren@users.noreply.github.com>
2025-01-27 22:47:58 +0000

    view the shopping cart

--- sky-server/src/main/java/com/sky/service/ShoppingCartService.java
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -1,6 +1,9 @@
 package com.sky.service;
 
 import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+
+import java.util.List;
 
 public interface ShoppingCartService {
 
@@ -10,5 +13,9 @@ public interface ShoppingCartService {
      */
     void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
 
-
+    /**
+     * view the shopping cart
+     * @return
+     */
+    List<ShoppingCart> showShoppingCart();
 }
